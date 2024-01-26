e4da45a973380debd144d18a44ba1b56d109fa0c
Gerson Aparecido Lima <gerson.lima@yduqs.com.br>
2024-01-26 17:03:02 -0300

    Limpando projeto

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// render e que vai renderiza o processamento do react
+//mas antes ele chama o css root
+//index.html
+// <div id="root"></div>
+//o react vai injeta todo o visual dentro deste div id="root"
+
+//render vai renderiza o resulta do componente  App
+//App fica neste local src\App.tsx
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
